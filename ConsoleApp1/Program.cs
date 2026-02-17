aee343275d607206372ea83f5dba3ff61e925829
ST10469816-Kudret-Akif-Boyaci <st10469816@vcconnect.edu.za>
2026-02-17 14:33:29 +0200

    Add files via upload

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,20 +1,22 @@
-﻿namespace ConsoleApp1  // toolbox
+﻿using System.Linq.Expressions;
+
+namespace ConsoleApp1  // toolbox
 {
 
     // tool 1
 
     class student
     {
-        public string name {  get; set; }
+        public string name { get; set; }
 
         public void greet()
         {
-            Console.WriteLine ($"hello my name is " {name});
+            Console.WriteLine($"hello my name is  { name}");
         }
     }
 
-        //tool 
-        //a teacher tool
+    //tool 
+    //a teacher tool
 
     class teacher
     {
@@ -22,13 +24,63 @@
 
         public void teach()
 
-            { Console.WriteLine($"{name} is teaching C# programming");
-    }
-    internal class Program
-    {
-        static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"{name} is teaching C# programming");
+        }
+
+        public class person // class
+        {
+            private string name;
+            private int age;
+            public person(string name, int age) // constructor
+            {
+                this.name = name;
+                this.age = age;
+
+            }
+
+            public void displayinfo()
+            {
+                Console.WriteLine($"name: {name}, age: {age}");
+            }
+        }
+        internal class Program // specific type of tool - hammer
+        {
+            static void Main(string[] args) // function of a hammer
+            {
+                // use theese tools
+                student student = new student();
+                student.name = "alice";
+
+                teacher teacher = new teacher();
+                teacher.name = "Mr. Smith";
+
+                student.greet();
+                teacher.teach();
+
+                Console.WriteLine("Hello, World!");
+
+                person person1 = new person("john", 40); // object instance
+                person person2 = new person("alice", 45); // object instance
+
+                person1.displayinfo();
+                person2.displayinfo();
+
+
+                try
+                {
+                    int x = 10;
+                    int y = 0;
+                    int result = x / y;
+
+                    Console.WriteLine( result);
+                }
+                catch (DivideByZeroException ex)
+                {
+                    Console.WriteLine("error " + ex.Message);
+                }
+
+            }
         }
     }
 }
