4d0ba5a926c296bf3f322f4ef6edc0f2d3209abc
ST10469816-Kudret-Akif-Boyaci <st10469816@vcconnect.edu.za>
2026-02-17 14:04:21 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ConsoleApp1  // toolbox
+{
+
+    // tool 1
+
+    class student
+    {
+        public string name {  get; set; }
+
+        public void greet()
+        {
+            Console.WriteLine ($"hello my name is " {name});
+        }
+    }
+
+        //tool 
+        //a teacher tool
+
+    class teacher
+    {
+        public string name { get; set; }
+
+        public void teach()
+
+            { Console.WriteLine($"{name} is teaching C# programming");
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
